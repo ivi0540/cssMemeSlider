44de59d96adddc28388644f6d9f96eda2aa354e5
ivi0540 <ivi0540@gmail.com>
2023-09-17 18:31:27 +0300

    feat: added animation to controllers (Sunday, Sep 17, 2023 18:31:12)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -3,13 +3,15 @@ let cont1 = document.querySelector("#cont1");
 let cont2 = document.querySelector("#cont2");
 let cont3 = document.querySelector("#cont3");
 let cont4 = document.querySelector("#cont4");
-
-
 let sliderTextMem = document.querySelector(".sliderTextMem");
 
-// let arr = [cont1, cont2, cont3, cont4];
-
 let widthImg = 699;
+let widthTextMem = 122;
+
+let mainColor = "#25282a";
+let colorController = "#71acff";
+let whiteColor = "#ffffff";
+
 
 function initSizeWindow() {
     if (window.innerWidth <= 1024) {
@@ -20,26 +22,90 @@ function initSizeWindow() {
 };
 
 initSizeWindow();
-
 window.addEventListener("resize", () => {
     initSizeWindow();
 });
 
-let widthTextMem = 122;
+function actionOneController() {
+    function actionController(cont) {
+        cont.style.backgroundColor = mainColor;
+    };
+    function deActionController(cont) {
+        cont.style.backgroundColor = colorController;
+    };
+    for (cont of arguments) {
+        deActionController(cont);
+    };
+    actionController(arguments[0]);
+};
+
+function pointingAtController(cont) {
+    cont.style.backgroundColor = whiteColor;
+    cont.style.border = "2px solid " + whiteColor;
+};
+
+function leavingOnController(cont) {
+    cont.style.backgroundColor = colorController;
+    cont.style.border = "2px solid " + colorController;
+};
+
+function pointingAtControllerBorder(cont) {
+    cont.style.border = "2px solid " + whiteColor;
+};
+
+function leavingOnControllerBorder(cont) {
+    cont.style.border = "2px solid " + colorController;
+};
+
+
+
+actionOneController(cont1, cont2, cont3, cont4);
 
 cont1.addEventListener("click", () => {
     slider.style.right = String(0) + "px";
     sliderTextMem.style.right = String(0) + "px";
+
+    actionOneController(cont1, cont2, cont3, cont4);
 });
 cont2.addEventListener("click", () => {
     slider.style.right = String(widthImg * 1) + "px";
     sliderTextMem.style.right = String(widthTextMem * 1) + "px";
+
+    actionOneController(cont2, cont1, cont3, cont4);
+
 });
 cont3.addEventListener("click", () => {
     slider.style.right = String(widthImg * 2) + "px";
     sliderTextMem.style.right = String(widthTextMem * 2) + "px";
+
+    actionOneController(cont3, cont1, cont2, cont4);
 });
 cont4.addEventListener("click", () => {
     slider.style.right = String(widthImg * 3) + "px";
     sliderTextMem.style.right = String(widthTextMem * 3) + "px";
+
+    actionOneController(cont4, cont1, cont2, cont3);
 });
+
+function pointControl(cont) {
+    cont.addEventListener("mouseover", () => {
+        if (cont.style.backgroundColor === "rgb(37, 40, 42)") {
+            pointingAtControllerBorder(cont);
+        } else {
+            pointingAtController(cont);
+        };
+    });
+
+    cont.addEventListener("mouseout", () => {
+        if (cont.style.backgroundColor === "rgb(37, 40, 42)") {
+            leavingOnControllerBorder(cont);
+        } else {
+            leavingOnController(cont);
+        };
+    });
+}
+
+pointControl(cont1);
+pointControl(cont2);
+pointControl(cont3);
+pointControl(cont4);
