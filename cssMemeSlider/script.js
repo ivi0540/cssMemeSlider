a1504dca7cc46d924004d7314d459dc6d9553265
ivi0540 <ivi0540@gmail.com>
2023-09-17 16:18:33 +0300

    feat: added a mobile version of the slider (Sunday, Sep 17, 2023 16:08:34)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -4,19 +4,42 @@ let cont2 = document.querySelector("#cont2");
 let cont3 = document.querySelector("#cont3");
 let cont4 = document.querySelector("#cont4");
 
-let arr = [cont1, cont2, cont3, cont4];
 
-const widthImg = 699;
+let sliderTextMem = document.querySelector(".sliderTextMem");
+
+// let arr = [cont1, cont2, cont3, cont4];
+
+let widthImg = 699;
+
+function initSizeWindow() {
+    if (window.innerWidth <= 1024) {
+        widthImg = 284;
+    } else {
+        widthImg = 699;
+    };
+};
+
+initSizeWindow();
+
+window.addEventListener("resize", () => {
+    initSizeWindow();
+});
+
+let widthTextMem = 122;
 
 cont1.addEventListener("click", () => {
     slider.style.right = String(0) + "px";
+    sliderTextMem.style.right = String(0) + "px";
 });
 cont2.addEventListener("click", () => {
     slider.style.right = String(widthImg * 1) + "px";
+    sliderTextMem.style.right = String(widthTextMem * 1) + "px";
 });
 cont3.addEventListener("click", () => {
     slider.style.right = String(widthImg * 2) + "px";
+    sliderTextMem.style.right = String(widthTextMem * 2) + "px";
 });
 cont4.addEventListener("click", () => {
     slider.style.right = String(widthImg * 3) + "px";
+    sliderTextMem.style.right = String(widthTextMem * 3) + "px";
 });
